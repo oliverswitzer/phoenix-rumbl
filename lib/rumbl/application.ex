21cb855efd9d508a33befcfc0dd21c5401519197
Oliver Switzer <oliverswitzer@gmail.com>
2020-10-06 11:48:00 -0400

    Create Ecto backed User

--- lib/rumbl/application.ex
+++ lib/rumbl/application.ex
@@ -7,8 +7,8 @@ defmodule Rumbl.Application do
 
   def start(_type, _args) do
     children = [
-#       Start the Ecto repository
-#      Rumbl.Repo,
+      # Start the Ecto repository
+      Rumbl.Repo,
       # Start the Telemetry supervisor
       RumblWeb.Telemetry,
       # Start the PubSub system
--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -3,27 +3,5 @@ defmodule Rumbl.Repo do
   In memory repo
   """
 
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
-      %Rumbl.User{id: "2", name: "Oliver", username: "oliverswitzer", password: "elixir"}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find(all(module), fn map -> map.id == id end)
-  end
-
-  def get_by(module, params) do
-    Enum.find(all(module), fn map ->
-      Enum.all?(
-        params,
-        fn {key, val} ->
-          Map.get(map, key) == val
-        end
-      )
-    end)
-  end
+  use Ecto.Repo, otp_app: :rumbl, adapter: Ecto.Adapters.Postgres
 end
